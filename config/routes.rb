b3d9b0fccf81c3ae0a49673cf7b2d03002f5d963
Masashi Nohara <chancy-m.n-72@ruri.waseda.jp>
2018-11-06 14:30:19 +0900

    completed backend on task manager

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,19 @@ Rails.application.routes.draw do
   # As a user, I can add a new task
   # As a user, I can edit a task (mark as completed / update title & details)
   # As a user, I can remove a task
+
+  # Create
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # Read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
